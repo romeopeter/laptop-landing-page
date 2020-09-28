ffffc29aade7d6686caca4def3ffa33d94a43470
Romeo-Peter <romeomeshack34@gmail.com>
2020-09-28 23:12:30 +0100

    Minimal  laptop landing page.

---
+++ app.js
@@ -0,0 +1,26 @@
+const navSlider = () => {
+	const burger = document.querySelector(".burger");
+	const nav = document.querySelector(".nav-links");
+	const navLinks = document.querySelectorAll('.nav-links li')
+
+	// Toggle nav
+	burger.addEventListener("click", () => {
+
+		// Burger animation
+		burger.classList.toggle("toggle");
+
+		// Slide-in mobile nav
+		nav.classList.toggle("nav-active");
+
+		navLinks.forEach((link, index) => {
+			if (link.style.animation) {
+				link.style.animation = "";
+			} else {
+				link.style.animation = `navLinkFade 0.5s ease-in-out forwards ${index / 7 + 0.2}s`;
+			}
+		});
+
+	});
+}
+
+navSlider();
